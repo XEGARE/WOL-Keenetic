dbc704dedf9737025b8e1a802a454a9954ee90a6
XEGARE <pilipzov@gmail.com>
2025-01-12 13:01:42 +0500

    add /status

--- WOL-Keenetic/app.js
+++ WOL-Keenetic/app.js
@@ -2,6 +2,7 @@ import express from 'express'
 import wol from 'wakeonlan'
 import dateFormat from 'dateformat'
 import config from './config.json' assert { type: 'json' }
+import ping from 'ping'
 
 function log(text) {
     console.log('[' + dateFormat(new Date(), "dd.mm.yyyy HH:MM:ss") + ']: ' + text)
@@ -17,7 +18,25 @@ app.get('/launch', (req, res) => {
         })
         .catch(err => {
             log('Error sending WOL: ' + err.message)
+            res.status(500).json({ status: 'error', message: err.message })
         })
+})
+
+app.get('/status', async (req, res) => {
+    try {
+        log(`Сhecking device status ${config.ipAddress}`)
+        const result = await ping.promise.probe(config.ipAddress)
+        if (result.alive) {
+            log('Device is online')
+            res.json({ value: true })
+        } else {
+            log('Device is offline')
+            res.json({ value: false })
+        }
+    } catch (err) {
+        log('Error pinging IP: ' + err.message)
+        res.status(500).json({ value: false, status: 'error', message: err.message })
+    }
 })
 
 app.listen(config.port, () => log(`Server has been started on port ${config.port}...`))
