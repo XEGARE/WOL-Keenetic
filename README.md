533e310b36360761293c148aca35c0b335d40a29
XEGARE <pilipzov@gmail.com>
2026-01-08 02:05:06 +0500

    New functionality for launching any device

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ This script, written in **Node.js**, sends a **magic packet** to a target device
 ## Installation Steps
 
 ### 1. Clone the repository and install Dependencies
+
 On your main system, clone the repository and install the required dependencies using npm:
 
 ```bash
@@ -13,18 +14,21 @@ cd WOL-Keenetic\WOL-Keenetic
 npm install
 ```
 
-### 2. Configure the config.json file
-Edit the config.json file to set up the following parameters:
+### 2. Configure the config file
 
-* **port**: Port for the web server.
-* **macAddress**: The MAC address of the device to be turned on.
-* **ipAddress**: The IP address of the device in the local network.
-* **secret**: Secret key for access.
+Edit the [`config.json`](./config.json) file to set up the following parameters:
+
+-   **port**: Port for the web server.
+-   **macAddress**: The MAC address of the device to be turned on.
+-   **ipAddress**: The IP address of the device in the local network.
+-   **secret**: Secret key for access.
 
 ### 3. Copy folders to Keenetic Router
+
 Copy the **WOL-Keenetic** folder and the **etc** directory to the Keenetic Entware path **/opt**.
 
 ### 4. Install Node.js on Keenetic via SSH
+
 Connect to your Keenetic router via SSH and install Node.js:
 
 ```bash
@@ -32,35 +36,71 @@ opkg install node
 ```
 
 ### 5. Set the script as executable
+
 Make the script executable using the following command:
+
 ```bash
 chmod +x /opt/etc/init.d/S55wol
 ```
 
 ### 6. Start the script
+
 Start the script using the init.d service:
+
 ```bash
 /opt/etc/init.d/S55wol start
 ```
 
 ### 7. Verify successful startup
+
 If the script starts successfully, you should see the following message:
+
 ```bash
 Process started successfully with PID №
 ```
 
 If any errors occur, refer to the logs for debugging:
+
 ```bash
 cat /opt/WOL-Keenetic/log.txt
 ```
 
 ### 8. Configure KeenDNS on the Keenetic Router
-Set up a domain name on the port from config.json and with access to the router.
 
-Use:  
-To start the device: [http://your.domain.name/launch/secret-key](http://your.domain.name/launch/secret-key)  
-To check the device status(see [Note](https://github.com/XEGARE/WOL-Keenetic?tab=readme-ov-file#note)): [http://your.domain.name/status/secret-key](http://your.domain.name/status/secret-key)
+Set up a domain name on the port from [`config.json`](./config.json) and with access to the router.
+
+### 9. Use
+
+#### Device from config:
+
+Launch:
+
+```
+http://<Your Keenetic Domain Name>/launch/<secret>
+```
+
+Status (see [Note](https://github.com/XEGARE/WOL-Keenetic?tab=readme-ov-file#note)):
+
+```
+http://<Your Keenetic Domain Name>/status/<secret>
+```
+
+#### Any device:
+
+Launch:
+
+```
+http://<Your Keenetic Domain Name>/launch/<secret>/<mac>/<ip>
+```
+
+Status (see [Note](https://github.com/XEGARE/WOL-Keenetic?tab=readme-ov-file#note)):
+
+```
+http://<Your Keenetic Domain Name>/status/<secret>/<ip>
+```
 
 ## Note
+
 ### For Windows Users
+
 For the proper functioning of the utility on Windows, it is necessary to allow the **ICMPv4** protocol in the firewall settings. This is required for operations involving network device availability checks (such as pinging).
--- WOL-Keenetic/app.js
+++ WOL-Keenetic/app.js
@@ -1,62 +1,146 @@
-import express from 'express'
-import wol from 'wakeonlan'
-import dateFormat from 'dateformat'
-import config from './config.json' assert { type: 'json' }
-import ping from 'ping'
+import dateFormat from "dateformat"
+import express from "express"
+import ping from "ping"
+import wol from "wakeonlan"
+import config from "./config.json" assert { type: "json" }
 
 function log(text) {
-    console.log('[' + dateFormat(new Date(), "dd.mm.yyyy HH:MM:ss") + ']: ' + text)
+    console.log(
+        "[" + dateFormat(new Date(), "dd.mm.yyyy HH:MM:ss") + "]: " + text
+    )
 }
 
 const app = express()
 
-app.get('/launch/:secret', (req, res) => {
+app.get("/launch/:secret", (req, res) => {
     const secretKey = req.params.secret
 
     if (!secretKey) {
-        return res.status(400).json({ status: 'error', message: 'Secret Key is required!' })
+        return res
+            .status(400)
+            .json({ status: "error", message: "Secret Key is required!" })
     }
 
     if (secretKey !== config.secret) {
-        return res.status(403).json({ status: 'error', message: 'Unauthorized access: Secret Key does not match!' })
+        return res.status(403).json({
+            status: "error",
+            message: "Unauthorized access: Secret Key does not match!",
+        })
     }
 
     wol(config.macAddress, { address: config.ipAddress })
         .then(() => {
-            log('WOL sent!')
-            res.json({ status: 'ok' })
+            log("WOL sent!")
+            res.json({ status: "ok" })
         })
-        .catch(err => {
-            log('Error sending WOL: ' + err.message)
-            res.status(500).json({ status: 'error', message: err.message })
+        .catch((err) => {
+            log("Error sending WOL: " + err.message)
+            res.status(500).json({ status: "error", message: err.message })
         })
 })
 
-app.get('/status/:secret', async (req, res) => {
+app.get("/launch/:secret/:mac/:address", (req, res) => {
     const secretKey = req.params.secret
+    const macAddress = req.params.mac
+    const ipAddress = req.params.address
 
     if (!secretKey) {
-        return res.status(400).json({ status: 'error', message: 'Secret Key is required!' })
+        return res
+            .status(400)
+            .json({ status: "error", message: "Secret Key is required!" })
     }
 
     if (secretKey !== config.secret) {
-        return res.status(403).json({ status: 'error', message: 'Unauthorized access: Secret Key does not match!' })
+        return res.status(403).json({
+            status: "error",
+            message: "Unauthorized access: Secret Key does not match!",
+        })
+    }
+
+    wol(macAddress, { address: ipAddress })
+        .then(() => {
+            log("WOL sent!")
+            res.json({ status: "ok" })
+        })
+        .catch((err) => {
+            log("Error sending WOL: " + err.message)
+            res.status(500).json({ status: "error", message: err.message })
+        })
+})
+
+app.get("/status/:secret", async (req, res) => {
+    const secretKey = req.params.secret
+
+    if (!secretKey) {
+        return res
+            .status(400)
+            .json({ status: "error", message: "Secret Key is required!" })
+    }
+
+    if (secretKey !== config.secret) {
+        return res.status(403).json({
+            status: "error",
+            message: "Unauthorized access: Secret Key does not match!",
+        })
     }
 
     try {
         log(`Сhecking device status ${config.ipAddress}`)
         const result = await ping.promise.probe(config.ipAddress)
         if (result.alive) {
-            log('Device is online')
+            log("Device is online")
             res.json({ value: true })
         } else {
-            log('Device is offline')
+            log("Device is offline")
             res.json({ value: false })
         }
     } catch (err) {
-        log('Error pinging IP: ' + err.message)
-        res.status(500).json({ value: false, status: 'error', message: err.message })
+        log("Error pinging IP: " + err.message)
+        res.status(500).json({
+            value: false,
+            status: "error",
+            message: err.message,
+        })
     }
 })
 
-app.listen(config.port, () => log(`Server has been started on port ${config.port}...`))
+app.get("/status/:secret/:address", async (req, res) => {
+    const secretKey = req.params.secret
+    const ipAddress = req.params.address
+
+    if (!secretKey) {
+        return res
+            .status(400)
+            .json({ status: "error", message: "Secret Key is required!" })
+    }
+
+    if (secretKey !== config.secret) {
+        return res.status(403).json({
+            status: "error",
+            message: "Unauthorized access: Secret Key does not match!",
+        })
+    }
+
+    try {
+        log(`Сhecking device status ${ipAddress}`)
+        const result = await ping.promise.probe(ipAddress)
+        if (result.alive) {
+            log("Device is online")
+            res.json({ value: true })
+        } else {
+            log("Device is offline")
+            res.json({ value: false })
+        }
+    } catch (err) {
+        log("Error pinging IP: " + err.message)
+        res.status(500).json({
+            value: false,
+            status: "error",
+            message: err.message,
+        })
+    }
+})
+
+app.listen(config.port, () =>
+    log(`Server has been started on port ${config.port}...`)
+)
